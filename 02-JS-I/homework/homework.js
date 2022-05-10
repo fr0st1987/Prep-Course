8b1723ade2c632ece538d9833dffa4f95adb6303
Joaco <joliveira1987@hotmail.com>
2022-05-10 13:01:16 -0300

    probando

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -188,7 +188,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return str=(str + '!')
+  return agregarSimboloExclamacion + '!'
 }
 
 function combinarNombres(nombre, apellido) {
