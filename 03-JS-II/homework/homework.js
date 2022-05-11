2b0801fde7b93f2a087d7fb18ef266089f9e96ac
Joaco <joliveira1987@hotmail.com>
2022-05-11 20:47:12 -0300

    and more

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -78,13 +78,14 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero == 10 || numero == 5
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-}
+return numero < 50 && numero > 20}
 
 function esEntero(numero) {
   // Devuelve "true" si "numero" es un entero (int/integer)
@@ -94,6 +95,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+if (numero === Math.floor(numero)) {
+  return true;
+}
+else return false
 }
 
 function fizzBuzz(numero) {
@@ -101,6 +106,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+if ((numero % 3) === 0) {
+    return 'fizz';
+}
+else if ((numero % 5) === 0) {
+    return 'buzz';
+}
+else if ((numero % 3) === 0) && ((numero % 5) === 0) 
+    { return 'fizzbuzz';
+}
+else {
+    return numero; 
+}
 }
 
 function operadoresLogicos(num1, num2, num3) {
