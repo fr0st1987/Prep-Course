8a36f7e459f3bb28921e256d054968fd937b95e8
Joaco <joliveira1987@hotmail.com>
2022-05-12 15:01:51 -0300

    in your face primos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -162,17 +162,21 @@ function fizzBuzz(numero) {
   else return false
 }
 
-  function esPrimo(numero) {
-    // Devuelve "true" si "numero" es primo
-    // De lo contrario devuelve "falso"
-    // Pista: un número primo solo es divisible por sí mismo y por 1
-    // Pista 2: Puedes resolverlo usando un bucle `for`
-    // Nota: Los números 0 y 1 NO son considerados números primos
-    for (let primos; (primos % primos) === 0 && (primos / 1) === primos; primos++) {
-      numero = numero + 1
-      return true
-    }
-  };
+function esPrimo(numero) {
+  // Devuelve "true" si "numero" es primo
+  // De lo contrario devuelve "falso"
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Pista 2: Puedes resolverlo usando un bucle `for`
+  // Nota: Los números 0 y 1 NO son considerados números primos
+if (numero === 0 || numero === 1) {
+    return false;
+}
+for (let i = 2; i < numero; i++) {
+if (((numero % i) === 0)) {
+      return false; }
+  else return true 
+}
+}
   
 
 function esVerdadero(valor){
