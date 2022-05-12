5584352b8809ed59c5f994df261f71f8869191d7
Joaco <joliveira1987@hotmail.com>
2022-05-12 11:42:22 -0300

    Faltan dos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -194,7 +194,7 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   for (let six = 0; six >= 60; six++ )
-  return.array 
+  return
 }
 
 function tieneTresDigitos(numero){
