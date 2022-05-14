2e130d1aa4c9ce53bc095d2242277dceb51a5922
Joaco <joliveira1987@hotmail.com>
2022-05-14 16:41:33 -0300

    Primer ejercicio

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,7 +3,7 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
-return devolverPrimerElemento[0]
+return devolverPrimerElemento(0)
 }
 
 
