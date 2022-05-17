5f4a6ab5b86f312173a2adf3b6c3ab1104b5ed41
Joaco <joliveira1987@hotmail.com>
2022-05-17 10:49:48 -0300

    a ver

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -39,7 +39,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
-  return array[array.unshift(elemento)]// Tu código:
+  array.unshift(elemento);
+  return array;// Tu código:
   }
 
 
@@ -49,14 +50,15 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-}
+return palabras.join(' ')}
 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-
+if ((array.indexOf(elemento)) != - 1) {
+  return true }
 }
 
 
